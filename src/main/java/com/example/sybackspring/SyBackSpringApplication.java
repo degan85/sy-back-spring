de5b964b028b26e3a0321dd498d64e5878860ae9
degan <degan8535@gmail.com>
2023-12-27 08:42:38 +0900

    first commit

---
+++ src/main/java/com/example/sybackspring/SyBackSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.sybackspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyBackSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyBackSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sybackspring/SyBackSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sybackspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyBackSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
